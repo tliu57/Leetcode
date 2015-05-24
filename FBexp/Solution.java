978b56be9328344c6f489e944532d4aeaf325ae8
Tianran <tliu57@asu.edu>
2015-05-24 10:51:30 -0700

    add facebook experience

---
+++ FBexp/Solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+	public String sortString(String order, String input) {
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+		for(int i = 0 ; i < order.length() ; i++) {
+			map.add(order.charAt(i), i);
+		}
+		int[] arr = new int[input.length()];
+		for(int i = 0 ; i < input.length(); i++) {
+			arr[i] = map.get(input.charAt(i));
+
+		}
+		return arr.toString();
+	}
+}
