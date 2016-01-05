187aab823f10240e583bf6a2773039b1d13363a6
Tianran <tliu57@asu.edu>
2016-01-05 15:25:14 -0700

    added a solution for StrobogrammaticNumber

---
+++ Easy/StrobogrammaticNumber/test.py
@@ -0,0 +1,17 @@
+class Solution(object):
+	def isStrobogrammatic(self, num):
+		dic = {"0":"0", "1":"1", "6":"9", "8":"8", "9":"6"}
+		l, r = 0, len(num) -1
+		while l <= r:
+			if num[l] not in dic or dic[num[l]] != num[r]:
+				return False
+			l += 1
+			r -= 1
+		return True
+
+
+
+
+sol = Solution()
+num = "6189"
+print sol.isStrobogrammatic(num)
--- Medium/threeSumSmaller/test.py
+++ Medium/threeSumSmaller/test.py
@@ -1,14 +1,13 @@
+nums = [-2, 0, 1, 3]
+target = 2
 ans = 0
 nums = sorted(nums)
 for i in xrange(len(nums)-2):
-	prev_ans = ans
 	j, k = i+1, len(nums)-1
 	while j<k:
 		if nums[i] + nums[j] + nums[k] < target:
-			ans += (k-j)
+			ans += (k - j)
 			j += 1
 		else:
 			k -= 1
-	if prev_ans == ans:
-		break
-return ans
+print ans
