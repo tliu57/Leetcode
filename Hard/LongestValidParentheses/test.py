b5d945a40a1b528d61391066749fe38dd5f2db81
Tianran <tliu57@asu.edu>
2016-01-14 23:42:42 -0800

    added a solution for longest valid parentheses

---
+++ Hard/LongestValidParentheses/test.py
@@ -0,0 +1,25 @@
+class Solution(object):
+	def longestValidParentheses(self, s):
+		if not s:
+			return 0
+		pos = range(0, len(s))
+		res = 0
+
+		for i in range(1, len(s)):
+			j = pos[i - 1]
+			print "i is:", i
+			print "j is:", j
+			if s[i] == ")" and j>=0 and s[j] == "(":
+				previous_start = pos[j - 1]
+				print "previous start is", previous_start
+				if previous_start <= j - 1:
+					pos[i] = previous_start
+				else:
+					pos[i] = j
+			print "pos[i] is", pos[i]
+			res = max(res, i - pos[i] + 1)
+		return res
+
+s = "())"
+sol = Solution()
+print sol.longestValidParentheses(s)
