e0cd56a58ab91b1c2ea00f6ede68ae580aa640ed
Tianran <tliu57@asu.edu>
2016-01-25 22:22:55 -0800

    added a solution for NQueens

--- Hard/NQueens/test.py
+++ Hard/NQueens/test.py
@@ -1,3 +1,4 @@
+import pdb
 class Solution():
 	def solveNQueens(self, n):
 		self.board = [-1 for i in range(n)]
@@ -10,15 +11,20 @@ class Solution():
 			self.res.append(valuelist)
 			return
 		for i in range(n):
+			pdb.set_trace()
 			if self.check(depth, i):
 				self.board[depth] = i
 				s = '.' * n
+				pdb.set_trace()
 				self.dfs(depth+1, valuelist+[s[:i]+'Q'+s[i+1:]], n)
 
 	def check(self, depth, Q_pos):
 		for i in range(depth):
+			pdb.set_trace()
 			if self.board[i] == Q_pos or abs(depth - i) == abs(self.board[i] - Q_pos):
+				pdb.set_trace()
 				return False
+		
 		return True
 
 sol = Solution()
