e726609b91af2b5d3808372a0f9e14a1a4eb5d57
Tianran <tliu57@asu.edu>
2016-02-16 13:25:28 -0800

    added a solution for wallsAndGate

--- Medium/wallsAndGate/test.py
+++ Medium/wallsAndGate/test.py
@@ -1,44 +1,31 @@
+import pdb
+INF = 2147483647
+
 class Solution(object):
-	def wallsAndGate(self, rooms):
+	def wallsAndGates(self, rooms):
+		self.que = []
 		for i in range(len(rooms)):
-			for j in range(len(rooms)):
+			for j in range(len(rooms[0])):
 				if rooms[i][j] == 0:
-					self.bfs(rooms, i, j)
-		print rooms
-
-	def bfs(self, rooms, i, j):
-		m = len(rooms)
-		n = len(rooms[0])
-		que = []
-		que.append((i, j))
-		while not que:
-			(ix, jx) = que.pop()
-			if isValid((ix-1, jx), rooms):
-				rooms[ix-1][jx] = rooms[ix][jx] + 1
-				que.append((ix-1, jx))
-			if isValid((ix+1, jx), rooms):
-				rooms[ix+1][jx] = rooms[ix][jx] + 1
-				que.append((ix+1, jx))
-			if isValid((ix, jx-1), rooms):
-				rooms[ix][jx-1] = rooms[ix][jx] + 1
-				que.append((ix, jx-1))
-			if isValid((ix, jx + 1), rooms):
-				rooms[ix][jx+1] = rooms[ix][jx] + 1
-				que.append((ix, jx+1))
-
-	def isValid(self, (ix, jx), rooms):
-		m = len(rooms)
-		n = len(rooms[0])
-		if ix < 0 or jx < 0 or ix > m-1 or jx > n-1 or rooms[ix][jx] == -1 or rooms[ix][jx] == 0:
-			return False
-		else:
-			return True
+					self.que.append((i, j))
+		dir = [[-1, 0, 1, 0],
+			[0, -1, 0, 1]]
+		while len(self.que) != 0:
+			(i, j) = self.que.pop()
+			for delta in range(len(dir[0])):
+			    new_i = i + dir[0][delta]
+			    new_j = j + dir[1][delta]
+			    if new_i < 0 or new_i >= len(rooms) or new_j < 0 or new_j >= len(rooms[0]) or rooms[new_i][new_j] == 0 or rooms[new_i][new_j] == -1:
+				continue
+			    if rooms[new_i][new_j] < rooms[i][j] + 1:
+			    	continue
+			    rooms[new_i][new_j] = rooms[i][j] + 1
+			    self.que.append((new_i,new_j))
+		return rooms
 
 sol = Solution()
-grid = [[3, -1, 0, 1],
-     	[2, 2, 1, -1],
-	[1, -1, 2, -1],
-	[0, -1, 3, 4]]
-
-print sol.wallsAndGate(grid)
-
+grid = [[INF, -1, 0, INF],
+     	[INF, INF, INF, -1],
+	[INF, -1, INF, -1],
+	[0, -1, INF, INF]]
+print sol.wallsAndGates(grid)
