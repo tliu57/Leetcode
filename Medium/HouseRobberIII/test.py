264c3f45243f812f3e79ee6c14c2d7a46d7d33a9
Tianran Liu <tianranliu@Tianrans-iMac.local>
2016-12-31 21:49:26 -0800

    modified the algorithm

--- Medium/HouseRobberIII/test.py
+++ Medium/HouseRobberIII/test.py
@@ -6,22 +6,20 @@ class TreeNode(object):
 
 class Solution(object):
 	def rob(self, root):
-		return self.robHelper(root, dict())
+		res = self.robHelper(root)
+		return max(res[0], res[1])
 
-	def robHelper(self, node, map):
+	def robHelper(self, node):
 		if not node:
-			return 0
-		if node in map.keys():
-			return map[node]
+			return [0]* 2
+		left_list = self.robHelper(node.left)
+		right_list = self.robHelper(node.right)
 
-		robMoney = 0
-		if node.left:
-			robMoney += self.robHelper(node.left.left, map) + self.robHelper(node.left.right, map)
-		if node.right:
-			robMoney += self.robHelper(node.right.left, map) + self.robHelper(node.right.right, map)
-		value = max(robMoney + node.val, self.robHelper(node.left, map) + self.robHelper(node.right, map))
-		map[node] = value
-		return value 
+		robMoney = [0]* 2
+		# first element stands for node house not being robbed, second element stands for house being robbed
+		robMoney[0] = max(left_list[0], left_list[1]) + max(right_list[0], right_list[1])
+		robMoney[1] = node.val + left_list[0] + right_list[0]
+		return robMoney 
 		
 
 
