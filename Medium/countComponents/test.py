83eb557ec90f06bd40d07fe3b95fac9210d0bc9e
Tianran Liu <tianranliu@Tianrans-iMac.local>
2017-01-06 08:39:33 -0800

    added a solution for count components

--- Medium/countComponents/test.py
+++ Medium/countComponents/test.py
@@ -1,13 +1,27 @@
+import collections
 class Solution(object):
 	def countComponents(self, n, edges):
 		edges.sort()
-		counter = 1
-		for i in range(1, len(edges)):
-			if edges[i][0] != edges[i-1][1]:
-				counter += 1
-		return counter
-
+		roots = [0] * n
+		for i in range(n):
+			roots[i] = i
+		for edge in edges:
+			root1 = self.findRoot(roots, edge[0])
+			root2 = self.findRoot(roots, edge[1])
+			if root1 != root2:
+				roots[root1] = root2
+				n -= 1
+		return n
+		
+	def findRoot(self, roots, id):
+		while roots[id] != id:
+			roots[id] = roots[roots[id]]
+			id = roots[id]
+		return id
+	
 sol = Solution()
-edges = [[0,1], [1,2], [2,3], [3,4]]
+edges = [[0,1], [1,2], [0,2], [3,4]]
 print sol.countComponents(5, edges)
 
+edges2 = [[0,1], [1,2], [3,4]]
+print sol.countComponents(5, edges2)
