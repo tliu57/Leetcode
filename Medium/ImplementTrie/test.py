72e24530cf1622ab228a96bf574e045596d33e25
Tianran Liu <tianranliu@Tianrans-iMac.local>
2017-02-25 17:16:37 -0800

    added a solution for implementing trie

--- Medium/ImplementTrie/test.py
+++ Medium/ImplementTrie/test.py
@@ -1,7 +1,7 @@
 class TrieNode(object):
 	def __init__(self):
-		self.node = defaultDict(TrieNode)
-		self.isWord = False
+		self.children = {}
+		self.Word = False
 
 class Trie(object):
 	def __init__(self):
@@ -10,21 +10,30 @@ class Trie(object):
 	def insert(self, word):
 		curr = self.root
 		for c in word:
-			curr = curr.node[c]
-		curr.isWord = True
+			if not c in curr.children:
+				curr.children[c] = TrieNode()
+			curr = curr.children[c]
+		curr.Word = True
 
 	def search(self, word):
 		curr = self.root
 		for c in word:
-			if c not in curr.nodes:
+			if c not in curr.children:
 				return False
-			curr = curr.nodes[c]
-		return curr.isWord
+			curr = curr.children[c]
+		return curr.Word
 
 	def startsWith(self, prefix):
 		curr = self.root
-                for c in word:
-		        if c not in curr.nodes:
+                for c in prefix:
+		        if c not in curr.children:
 				return False
-			curr = curr.nodes[c]
+			curr = curr.children[c]
 		return True
+
+obj = Trie()
+obj.insert("Test")
+print obj.search("Test")
+print obj.search("Testt")
+print obj.startsWith("Te")
+
