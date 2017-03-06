a1107bc09618f2a2332290bbe4bc3c6a598b84e5
Tianran Liu <tliu57@asu.edu>
2017-03-05 21:39:13 -0800

    add bfs to solution

--- Medium/MaximumDepthOfBinaryTree/test.py
+++ Medium/MaximumDepthOfBinaryTree/test.py
@@ -9,7 +9,26 @@ class Solution(object):
 		if not root:
 			return 0
 		return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
-
+	
+	def maxDepthDFS(self, root):
+		if not root:
+			return 0
+		depth = 0
+		queue = []
+		queue.append(root)
+		while queue:
+			depth += 1
+			n = len(queue)
+			for _ in range(n):
+				node = queue.pop(0)
+				if node.left:
+					queue.append(node.left)
+				if node.right:
+					queue.append(node.right)
+		return depth
+				
+		
+	
 sol = Solution()
 node1 = TreeNode(7)
 node2 = TreeNode(3)
@@ -20,3 +39,4 @@ node1.right = node3
 node2.right = node4
 
 print sol.maxDepth(node1)
+print sol.maxDepthDFS(node1)
