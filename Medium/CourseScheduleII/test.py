e7355b7e661fed25d9f18bdcd005c3168a440647
Tianran Liu <tliu57@asu.edu>
2017-04-20 18:27:46 -0700

    test

--- Medium/CourseScheduleII/test.py
+++ Medium/CourseScheduleII/test.py
@@ -1,23 +1,25 @@
 class Solution(object):
 	def findOrder(self, numCourses, prerequisites):
 		graph = self.makeGraph(numCourses, prerequisites)
+		print "graph is:", graph
 		degrees = self.computeIndegree(graph)
-		order = [0] * numCourses
+		print "degree is:", degrees
+		order = []
 		toVisit = []
 		for i in range(len(degrees)):
 			if degrees[i] == 0:
 				toVisit.append(i)
-
-		visited = 0
 		while toVisit:
+			print "toVisite is:", toVisit
 			fromNode = toVisit.pop()
-			order[visited] = fromNode
-			visited += 1
+			print "fromNode is:", fromNode
+			order.append(fromNode)
 			for toNode in graph[fromNode]:
 				degrees[toNode] -= 1
 				if degrees[toNode] == 0:
 					toVisit.append(toNode)
-		return order if visited == len(degrees) else []
+		print "order is:", order
+		return order if len(order) == len(degrees) else []
 
 	def makeGraph(self, numCourses, prerequisites):
 		graph = []
