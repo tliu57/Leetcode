4a43b9949855a7de209566813da3d0f2b909c302
Tianran Liu <tliu57@asu.edu>
2017-06-11 11:27:32 -0700

    added a solution for flatten 2d vector

---
+++ Airbnb/Flatten2DVector/test.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Vector2D {
+	vector<vector<int>>::iterator i, iEnd;
+	int j = 0;
+
+public:
+	Vector2D(vector<vector<int>>& vec2d) {
+		i = vec2d.begin();
+		iEnd = vec2d.end();
+	}
+
+	int next() {
+		if (hasNext()) {
+			return (*i)[j++];
+		}
+		else return -1;
+	}
+
+	bool hasNext() {
+		if (i != iEnd and j == (*i).size()) {
+			i++;
+			j = 0;
+		}
+		return i != iEnd;	
+	}
+};
+
+int main() {
+	vector<vector<int>> vec2d;
+	vec2d.push_back({1,2});
+	vec2d.push_back({3});
+	vec2d.push_back({4, 5, 6});
+	Vector2D j(vec2d);
+	vector<vector<int>> vec2dWithEmpty;
+	vec2dWithEmpty.push_back({1});
+	vec2dWithEmpty.push_back({});
+	Vector2D i(vec2dWithEmpty);
+	while (i.hasNext()) cout << i.next();
+}
