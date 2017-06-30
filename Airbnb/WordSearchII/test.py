804fd122b527099905d8c3939f6292b246494b9b
Tina Liu <tianran_liu@apple.com>
2017-06-30 16:46:43 -0700

    add solution

---
+++ Airbnb/WordSearchII/test.py
@@ -0,0 +1,72 @@
+class TrieNode(object):
+	def __init__(self):
+		self.children = {}
+		self.word = False
+
+class Trie(object):
+	def __init__(self):
+		self.root = TrieNode()
+
+	def insert(self, word):
+		curr = self.root
+		for c in word:
+			if not c in curr.children:
+				curr.children[c] = TrieNode()
+			curr = curr.children[c]
+		curr.word = True
+
+	def search(self, word):
+		curr = self.root
+		for c in word:
+			if c not in curr.children:
+				return False
+			curr = curr.children[c]
+		return curr.word
+
+class Solution(object):
+	def buildTrie(self, words):
+		trie = Trie()
+		for word in words:
+			trie.insert(word)
+		return trie.root
+
+	def findWords(self, board, words):
+		res= []
+		root = self.buildTrie(words)
+		m = len(board)
+		n = len(board[0])
+		for i in range(m):
+			for j in range(n):
+				self.dfs(board, i, j, root, res)
+		return res
+
+	def dfs(self, board, i, j, node, res):
+		c = board[i][j]
+		if c == '#' or (c - 'a') not in node.children:
+			return
+		node = node.children[c - 'a']
+		if node.word:
+			res.append(node.word)
+		board[i][j] = "#"
+		if i > 0:
+			self.dfs(board, i-1, j, node, res)
+		if j > 0:
+			self.dfs(board, i, j-1, node, res)
+		if i < len(board) - 1:
+			self.dfs(board, i+1, j, node, res)
+		if j < len(board[0]) - 1:
+			self.dfs(board, i, j+1, node, res)
+		board[i][j] = c
+
+
+
+sol = Solution()
+words = ["oath", "pea", "eat", "rain"]
+root = sol.buildTrie(words)
+queue = [root]
+while queue:
+	print queue
+	new_level = []
+	for node in queue:
+		new_level.append(node.children.keys())
+	queue = new_level
