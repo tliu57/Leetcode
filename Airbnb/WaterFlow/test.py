901a04a5951933c06416fbeecec0484efb9c080d
Tianran Liu <tliu57@asu.edu>
2017-07-04 18:01:40 -0700

    water flow

--- Airbnb/WaterFlow/test.py
+++ Airbnb/WaterFlow/test.py
@@ -1,19 +1,35 @@
-class Solution(object):
-	def pour_water(self, bumps, waterVolume):
-		waters = [0] * len(bumps)
-		water_to_pour = waterVolume
-		while water_to_pour:
-			for i in range(len(waters)-1):
-				if bumps[i] + waters[i] + 1 <= bumps[i+1] + waters[i+1]:
-					waters[i] += 1
-					water_to_pour -= 1
-					break
-		return waters
+def pour_water(terrains, location, water):
+    waters = [0] * len(terrains)
+    while water > 0:
+        left = location - 1
+        while left >= 0:
+            if terrains[left] + waters[left] > terrains[left + 1] + waters[left + 1]:
+                break
+            left -= 1
 
-sol = Solution()
-bumps = [5, 4, 2, 1, 2, 3, 2, 1, 0, 1, 2, 4]
-waterVolume = 8
-print sol.pour_water(bumps, waterVolume)
-			
-			
-		
+        if terrains[left + 1] + waters[left + 1] < terrains[location] + waters[location]:
+            location_to_pour = left + 1
+        else:
+            right = location + 1
+            while right < len(terrains):
+                if terrains[right] + waters[right] > terrains[right - 1] + waters[right - 1]:
+                    break
+                right += 1
+
+            if terrains[right - 1] + waters[right - 1] < terrains[location] + waters[right - 1]:
+                location_to_pour = right - 1
+            else:
+                location_to_pour = location
+
+        waters[location_to_pour] += 1
+
+        print "location_to_pour:", location_to_pour
+        water -= 1 
+
+    return waters
+
+
+terrains = [5, 4, 2, 1, 2, 3, 2, 1, 0, 1, 2, 4]
+water = 8
+location = 0
+pour_water(terrains, location, water)
