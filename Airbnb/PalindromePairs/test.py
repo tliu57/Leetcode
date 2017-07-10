02db9af94d6781a5b4ff4b4b5ecc001bd4522f4b
Tianran Liu <tliu57@asu.edu>
2017-07-09 21:50:59 -0700

    fix python solution for palindrome pairs

--- Airbnb/PalindromePairs/test.py
+++ Airbnb/PalindromePairs/test.py
@@ -5,29 +5,20 @@ class Solution(object):
 		for i in range(len(words)):
 			dict[words[i]] = i
 		for i in range(len(words)):
-			for j in range(len(words[i])):
+			for j in range(len(words[i])+1):
 				left = words[i][:j]
 				right = words[i][j:]
-				if self.isPalindrome(left):
-					right_rev = right[::-1]
-					if right_rev in dict and dict[right_rev]!= i:
-						res.append([dict[right_rev], i])
-				if self.isPalindrome(right):
-					left_rev = left[::-1]
-					if left_rev in dict and dict[left_rev] != i:
-						res.append([i, dict[left_rev]])
+				right_rev = right[::-1]
+				left_rev = left[::-1]
+				if left_rev in dict and dict[left_rev]!= i and right == right[::-1]:
+					res.append([i, dict[left_rev]])
+				if j > 0 and right_rev in dict and dict[right_rev] != i and left == left[::-1]:
+					res.append([dict[right_rev], i])
 		return res
 
-	def isPalindrome(self, word):
-		i = 0
-		j = len(word) - 1
-		while i < j:
-			if word[i]!=word[j]:
-				return False
-			i += 1
-			j -= 1
-		return True
 
 sol = Solution()
 words = ["abcd", "dcba", "lls", "s", "sssll"]
+corner = ["a", ""]
 print sol.palindromePairs(words)
+
