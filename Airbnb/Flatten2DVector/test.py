859b9104f3efcadd28502ff730f36d6ebd542daa
Tianran Liu <tliu57@asu.edu>
2017-08-31 09:41:33 -0700

    adding a solution for flatten 2d vector

--- Airbnb/Flatten2DVector/test.py
+++ Airbnb/Flatten2DVector/test.py
@@ -6,21 +6,44 @@ vector = [
 
 class Vector2D(object):
 	def __init__(self, vec2d):
+		self.prevRow = None
+		self.prevCol = None
 		self.row = 0
 		self.col = 0
 		self.vector = vec2d
+		self._moveToNext()
 	
-	def next(self):
-		val = self.vector[self.row][self.col]
-		self.col += 1
-		return val
-	
-	def hasNext(self):
-		while self.row != len(self.vector) and self.col == len(self.vector[self.row]):
+	def _moveToNext(self):
+		while self.row < len(self.vector):
+			if self.col < len(self.vector[self.row]):
+				break
 			self.row += 1
 			self.col = 0
-		return self.row != len(self.vector)
 
+	def next(self):
+		if self.hasNext():
+			self.prevRow = self.row
+			self.prevCol = self.col
+			self.col += 1
+			self._moveToNext()
+			return self.vector[self.prevRow][self.prevCol]
+		else:
+			return None
+	
+	def hasNext(self):
+		return self.row < len(self.vector)
+
+	def remove(self):
+		if self.prevRow and self.prevCol:
+			ls = self.vector[self.prevRow]
+			self.vector[self.prevRow] = ls[:self.prevCol] + ls[self.prevCol+1:]
+			self.row = self.prevRow
+			self.col = self.prevCol
+			self._moveToNext()
+			self.prevRow = None
+			self.prevCol = None
+		else:
+			raise Exception("called remove before next!")
 
 i, v = Vector2D(vector), []
 while i.hasNext():
