2d13248313afd3f8785b8ca867143e303ecdc7c7
Tianran Liu <tliu57@asu.edu>
2017-10-21 23:55:14 -0700

    add a solution for sliding window

--- Google/SlidingWindowAverage/test.py
+++ Google/SlidingWindowAverage/test.py
@@ -2,16 +2,17 @@ class MovingAverage(object):
 	def __init__(self, size):
 		self.id = 0
 		self.size = size
-		self.sum = []
-		self.sum.append(0)
+		self.sum = [0 for i in range(size+1)]
 
 	def next(self, val):
-		self.sum.append(self.sum[-1] + val)
 		self.id += 1
+		curr_idx = self.id % (self.size + 1)
+		prev_idx = (self.id - 1) % (self.size + 1) 
+		self.sum[curr_idx] = self.sum[prev_idx] + val
 		if self.id > self.size:
-			return float(self.sum[self.id] - self.sum[self.id - self.size])/ self.size
+			return float(self.sum[curr_idx] - self.sum[(self.id - self.size) % (self.size + 1)])/ self.size
 		else:
-			return float(self.sum[self.id]) / self.id
+			return float(self.sum[curr_idx]) / self.id
 
 movAverage = MovingAverage(3)
 print movAverage.next(1)
