c48adc8e27ffe91d4ccd9cf82ba84125067b6a5d
Tianran Liu <tliu57@asu.edu>
2017-12-09 22:41:24 -0800

    solution for Basic calculator two

---
+++ Twitter/BasicCalculatorII/testcase.java
@@ -0,0 +1,10 @@
+import java.util.*;
+  
+public class testcase {
+        public static void main(String[] args) {
+                Solution answer = new Solution();
+                String str = "14-3/2";
+                int result = answer.calculate(str);
+                System.out.println(result);
+        }
+}
