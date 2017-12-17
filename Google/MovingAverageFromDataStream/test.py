2722ae0028edff3dd4d567c109cc6ab68f033287
Tianran Liu <tliu57@asu.edu>
2017-12-16 21:37:45 -0800

    use a rotated array

--- Google/MovingAverageFromDataStream/test.py
+++ Google/MovingAverageFromDataStream/test.py
@@ -1,16 +1,18 @@
 class MovingAverage(object):
     def __init__(self, size):
-	self.sum = 0
+	self.sum = [0 for i in range(size + 1)]
+	self.index = 0
 	self.size = size
-	self.window = []
+
+    def mod(self, val):
+	return val % (self.size + 1)
 
     def next(self, val):
-	self.sum += val
-	self.window.append(val)
-	if len(self.window) > self.size:
-		self.sum -= self.window[0]
-		self.window.pop(0)
-	return self.sum/len(self.window)
+	self.index += 1
+	self.sum[self.mod(self.index)] = self.sum[self.mod(self.index - 1)] + val
+	if self.index > self.size:
+		return float(self.sum[self.mod(self.index)] - self.sum[self.mod(self.index - self.size)]) / float(self.size)
+	return float(self.sum[self.mod(self.index)])/float(self.index)
 
 
 mov = MovingAverage(3)
