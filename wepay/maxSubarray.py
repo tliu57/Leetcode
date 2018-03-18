56d9b9aab8a420f6b8076a119846bcaeede3b5da
Tianran Liu <tliu57@asu.edu>
2018-03-17 23:13:51 -0700

    max subarray

--- wepay/maxSubarray.py
+++ wepay/maxSubarray.py
@@ -1,11 +1,12 @@
 class Solution(object):
 	def maxSubArray(self, nums):
 		dp = [0 for i in range(len(nums))]
-		dp[0] = nums[0] + nums[1]
+		dp[1] = nums[0] + nums[1]
 		maxSum = dp[0]
 		for i in range(2, len(nums)):
-			incr = nums[i] - nums[i-2]
-			dp[i] = incr + dp[i-1] if incr > 0 else dp[i-1]
+			dp[i] = nums[i] + nums[i-1]
+			if dp[i-1] - nums[i-1] > 0: 
+				dp[i] += dp[i-1] - nums[i-1]
 			maxSum = max(maxSum, dp[i])
 		return maxSum
 
